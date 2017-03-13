a016a0605a6656a4263b38431d5c171cb0f08624
Kassang Konzi <kassang.konzi@backelite.com>
2017-03-13 12:03:00 +0100

    Broke collect_manifest_info method into many thinner ones

--- lib/apk_analyzer/analyzer.rb
+++ lib/apk_analyzer/analyzer.rb
@@ -28,68 +28,103 @@ module ApkAnalyzer
         manifest_info[:path_in_apk] = find_file_in_apk('AndroidManifest.xml')
         content = {}
         # application content
-        application_content = {}
-        application_name = @manifest_xml.xpath('//application/@android:name')
-        application_content[:application_name] = application_name
-        application_id = @manifest_xml.xpath('//manifest/@package')
-        application_content[:application_id] = application_id
-        intent_filters = @manifest_xml.xpath('//intent-filter')
-        puts intent_filters
-        intents = []
-        intent_filters.each do |intent|
-          intent_attributes = {}
-          actions = []
-          category = nil
-          intent.children.each do |child|
-            puts child if child.is_a?(Nokogiri::XML::Element)
-            next unless child.is_a?(Nokogiri::XML::Element)
-            if child.name == 'action'
-              actions.push child.attributes['name'].value
-            elsif child.name == 'category'
-              category = child.attributes['name'].value
-            end
-            intent_attributes[:actions] = actions
-            intent_attributes[:category] = category
-          end
-          intents.push intent_attributes
-        end
-        content[:intents] = intents
+        content[:application_info] = collect_application_info(@manifest_xml)
+
+        # intents
+        content[:intents] = collection_intent_info(@manifest_xml)
+
         # sdk infos
-        minimum_sdk_version = @manifest_xml.xpath('//uses-sdk/@android:minSdkVersion')
-        target_sdk_version = @manifest_xml.xpath('//uses-sdk/@android:targetSdkVersion')
-        data = [minimum_sdk_version, target_sdk_version].map { |elt| sanitize_hex(elt.first.value) unless elt.empty? }
-        content[:uses_sdk] = { minimum_sdk_version: data[0], target_sdk_version: data[1] }
+        sdk_infos = collect_sdk_info(@manifest_xml)
+        content[:uses_sdk] = { minimum_sdk_version: sdk_infos[0], target_sdk_version: sdk_infos[1] }
 
         # uses permission
-        content[:uses_permissions] = []
-        @manifest_xml.xpath('//uses-permission/@android:name').each { |permission| content[:uses_permissions].push permission.value }
+        uses_permissions = collect_uses_permission_info(@manifest_xml)
+        content[:uses_permissions] = uses_permissions
 
         # uses features
-        content[:uses_features] = []
-        features = @manifest_xml.xpath('//uses-feature')
-        feature_list = []
-        features.each do  |feature|
-          feature_element = {}
-          feature.attributes.each_value do |attr|
-            value = attr.value
-            value = bool_conv(value) if attr.name == 'required'
-            if attr.name == 'glEsVersion'
-              feature_element[:name] = open_gl_version_conv(attr.value)
-            else
-              feature_element[attr.name.to_sym] = value
-            end
-          end
-          feature_list.push feature_element
-        end
+        feature_list = collect_uses_feature_info(@manifest_xml)
         content[:uses_features] = feature_list
 
         # screen compatibility
-        content[:supports_screens] = []
-        screen_types = @manifest_xml.xpath('//supports-screens').first
-        screen_types.attributes.each { |attr_name, attr_object| content[:supports_screens].push attr_name unless attr_object.value == FALSE } unless screen_types.nil?
+        supported_screens = collect_supported_screens(@manifest_xml)
+        content[:supports_screens] = supported_screens
+
         manifest_info[:content] = content
       end
     end
+
+    def collect_supported_screens(manifest_xml)
+      supported_screens = []
+      screen_types = manifest_xml.xpath('//supports-screens').first
+      unless screen_types.nil?
+        screen_types.attributes.each { |attr_name, attr_object| supported_screens.push attr_name unless attr_object.value == FALSE }
+      end
+      supported_screens
+    end
+
+    def collect_uses_feature_info(manifest_xml)
+      features = manifest_xml.xpath('//uses-feature')
+      feature_list = []
+      features.each do  |feature|
+        feature_element = {}
+        feature.attributes.each_value do |attr|
+          value = attr.value
+          value = bool_conv(value) if attr.name == 'required'
+          if attr.name == 'glEsVersion'
+            feature_element[:name] = opengl_version_conv(attr.value)
+          else
+            feature_element[attr.name.to_sym] = value
+          end
+        end
+        feature_list.push feature_element
+      end
+      feature_list
+    end
+
+    def collect_uses_permission_info(manifest_xml)
+      uses_permissions = []
+      manifest_xml.xpath('//uses-permission/@android:name').each { |permission| uses_permissions.push permission.value }
+      uses_permissions
+    end
+
+    def collect_application_info(manifest_xml)
+      application_content = {}
+      application_name = manifest_xml.xpath('//application/@android:name')
+      application_content[:application_name] = application_name
+      application_id = manifest_xml.xpath('//manifest/@package')
+      application_content[:application_id] = application_id
+      application_content
+    end
+
+    def collection_intent_info(manifest_xml)
+      intent_filters = manifest_xml.xpath('//intent-filter')
+      intents = []
+      intent_filters.each do |intent|
+        intent_attributes = {}
+        actions = []
+        category = nil
+        intent.children.each do |child|
+          next unless child.is_a?(Nokogiri::XML::Element)
+          if child.name == 'action'
+            actions.push child.attributes['name'].value
+          elsif child.name == 'category'
+            category = child.attributes['name'].value
+          end
+        end
+        intent_attributes[:actions] = actions unless actions.empty?
+        intent_attributes[:category] = category unless category.nil?
+        intents.push intent_attributes
+      end
+      intents
+    end
+
+    def collect_sdk_info(manifest_xml)
+      minimum_sdk_version = manifest_xml.xpath('//uses-sdk/@android:minSdkVersion')
+      target_sdk_version = manifest_xml.xpath('//uses-sdk/@android:targetSdkVersion')
+      sdk_infos = [minimum_sdk_version, target_sdk_version].map { |elt| sanitize_hex(elt.first.value) unless elt.empty? }
+      sdk_infos
+    end
+
 
     private
 
